848311cbe11bfb1315425a41e7e80128942a058c
mdstoy <toyofyou.me@gmail.com>
2014-05-06 02:23:00 +0900

    apply event page

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.extension.onRequest.addListener(
+chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse){
 		if(request.gesture == 'rl'){
 			chrome.tabs.remove(sender.tab.id);
