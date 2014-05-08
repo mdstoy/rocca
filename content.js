0b1c4cef67f44ea794c8bfe46900dd61ea2be985
mdstoy <toyofyou.me@gmail.com>
2014-05-09 02:33:46 +0900

    feature JQuery

--- content.js
+++ content.js
@@ -6,48 +6,49 @@ var canShowContext = false;
 var lmousedown = false;
 var rmousedown = false;
 
-window.addEventListener('mousedown', function(e){
-
-	//console.log("down:" + e.button);
-	if(e.button == L_BUTTON){
-		lmousedown = true;
-	}else if(e.button == R_BUTTON){
-		rmousedown = true;
-	}
-
-	if(e.button == L_BUTTON && rmousedown){
-		execGesture("rl");
-		lmousedown = false;
-	}else if(e.button == R_BUTTON && lmousedown){
-		execGesture("lr");
-		rmousedown = false;
-		canShowContext = false;
-	}
-});
-
-window.addEventListener('mouseup', function(e){
-
-	//console.log("up:" + e.button);
-	if(e.button == L_BUTTON){
-		lmousedown = false;
-		canShowContext = false;
-	}else if(e.button == R_BUTTON){
-		if(rmousedown){
-			rmousedown = false;
-			canShowContext = true;
+$(function(){
+	$(document).on('mousedown', function(e){
+		//console.log("down:" + e.button);
+		if(e.button == L_BUTTON){
+			lmousedown = true;
+		}else if(e.button == R_BUTTON){
+			rmousedown = true;
 		}
-	}
-});
+	
+		if(e.button == L_BUTTON && rmousedown){
+			execGesture("rl");
+			lmousedown = false;
+		}else if(e.button == R_BUTTON && lmousedown){
+			execGesture("lr");
+			rmousedown = false;
+			canShowContext = false;
+		}
+	});
 
-window.addEventListener('contextmenu', function(e){
-	if(canShowContext){
-		// after shown contextmenu, reset mouse state
-		rmousedown = false;
-		canShowContext = false;
-	}else{
-		e.preventDefault();
-		e.stopPropagation();
-	}
+	$(document).on('mouseup', function(e){
+		//console.log("up:" + e.button);
+		if(e.button == L_BUTTON){
+			lmousedown = false;
+			canShowContext = false;
+		}else if(e.button == R_BUTTON){
+			if(rmousedown){
+				rmousedown = false;
+				canShowContext = true;
+			}
+		}
+	});
+
+	$(document).on('contextmenu', function(e){
+		if(canShowContext){
+			// after shown contextmenu, reset mouse state
+			rmousedown = false;
+			canShowContext = false;
+		}else{
+			e.preventDefault();
+			e.stopPropagation();
+		}
+	});
+
 });
 
 function execGesture(action){
