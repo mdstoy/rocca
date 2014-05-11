13e40965f74eb63892759bfbb884a72b11f52432
mdstoy <toyofyou.me@gmail.com>
2014-05-11 23:38:03 +0900

    fixes #12

--- options.js
+++ options.js
@@ -1,13 +1,38 @@
+function createRows(data){
+
+	var rows = $.each(data, function(index, value){
+		var row = '<tr><td id="header_' + value + '"></td>'
+			+ '<td><input type="radio" name="rl" value="' + value + '" id="rl_' + value + '"></td>'
+			+ '<td><input type="radio" name="lr" value="' + value + '" id="lr_' + value + '"></td>'
+			+ '<td><input type="text" name="' + value + '"></td>'
+			+ '</tr>';
+
+		$('tbody').append(row);
+		$('#header_' + value).text(chrome.i18n.getMessage(toCamel(value)));
+	});
+}
+
+function toCamel(snake){
+	return snake.replace(/_./g, function(matched){
+		return matched.charAt(1).toUpperCase();
+	});
+}
+
 $(function(){
+
+	var data = [
+		"tab_remove"
+		, "history_back"
+		, "history_forward"
+		, "reload"
+	];
+
+	createRows(data);
+
 	chrome.storage.sync.get({lr: 'history_back', rl: 'tab_remove'}, function(items){
 		$('#rl_' + items.rl).prop('checked', true);
 		$('#lr_' + items.lr).prop('checked', true);
 	});
-
-	$('#header_tab_remove').text(chrome.i18n.getMessage("tabRemove"));
-	$('#header_history_back').text(chrome.i18n.getMessage("historyBack"));
-	$('#header_history_forward').text(chrome.i18n.getMessage("historyForward"));
-	$('#header_reload').text(chrome.i18n.getMessage("reload"));
 
 	// onclick
 	$('#save').click(function(){
